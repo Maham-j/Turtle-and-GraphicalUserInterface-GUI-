268d7780ed15ffc229d13e62c3f13836acbffae1
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-08-26 19:49:06 +0500

    Create My-Name.py

---
+++ My-Name.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+
+'''for my name initial'''
+timmy.shape('turtle')
+timmy.color("purple")
+timmy.left(90)
+timmy.forward(100)
+timmy.right(140)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.right(140)
+timmy.forward(100)
